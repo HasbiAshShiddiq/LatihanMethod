12eb8c3dde48ae7b178d7e7a5fbcea0eb3707f1e
HasbiAshShiddiq <abi.hasbiashshiddiq@gmail.com>
2019-04-02 14:34:25 +0700

    LatihanMethod

---
+++ Method/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarasi Variabel
+            int hasil;
+            
+            //Membuat Objek  Dari Class Calculator
+            Calculator calculator = new Calculator();
+
+            //Panggil Method Penjumlahan
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            //Panggil Method Penjumlah Dengan 3 Parameter (Overload Method)
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil Penjumlah Overload : " + hasil);
+
+            //Panggil Method Pengurangan
+            hasil = calculator.Pengurangan(7, 3);
+            CetakHasil("Hasil Pengurangan : " + hasil);
+
+            //Panggil Static Method, Langsung Dari Nama Classnya
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian : " + hasil);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
